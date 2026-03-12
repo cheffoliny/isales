82ca7f35b2a8e4fdee15dbf94b585a064731f31d
S34083 <stefan.milanov@telepol.com>
2026-03-12 15:17:08 +0200

    СНИМКИ

--- includes/items_fetch.php
+++ includes/items_fetch.php
@@ -1,35 +1,49 @@
 <?php
 include_once __DIR__.'/functions.php';
-$db=db_connect('storage');
+$db = db_connect('storage');
 
-$page=(int)($_GET['page']??0);
-$search=trim($_GET['search']??'');
-$promo=(int)($_GET['promo']??0);
-$zero=(int)($_GET['zero']??0);
-$limit=50;
-$offset=$page*$limit;
+$page = (int)($_GET['page'] ?? 0);
+$search = trim($_GET['search'] ?? '');
+$promo = (int)($_GET['promo'] ?? 0);
+$zero = (int)($_GET['zero'] ?? 0);
+$limit = 50;
+$offset = $page * $limit;
 
-$where="WHERE n.to_arc=0";
-if($search!==''){
-    $s=$db->real_escape_string($search);
-    $where.=" AND (n.nom_code LIKE '%$s%' OR n.name LIKE '%$s%')";
+$where = "WHERE n.to_arc=0";
+if ($search !== '') {
+    $s = $db->real_escape_string($search);
+    $where .= " AND (n.nom_code LIKE '%$s%' OR n.name LIKE '%$s%')";
 }
-if($promo){ $where.=" AND n.sales_price>0"; }
-if($zero){ $where.=" AND n.is_calc=0"; }
+if ($promo) { $where .= " AND n.sales_price>0"; }
+if ($zero) { $where .= " AND n.is_calc=0"; }
 
-$sql="SELECT n.id, n.nom_code, n.name, n.client_price, n.sales_price, n.is_calc, n.image 
-      FROM nomenclatures n $where 
-      ORDER BY n.nom_code 
-      LIMIT $limit OFFSET $offset";
+$sql = "SELECT n.id, n.nom_code, n.name, n.client_price, n.sales_price, n.is_calc, n.image
+        FROM nomenclatures n
+        $where
+        ORDER BY n.nom_code
+        LIMIT $limit OFFSET $offset";
 
-$res=$db->query($sql);
-$html='';
+$res = $db->query($sql);
+$html = '';
 
-while($r=$res->fetch_assoc()){
+while ($r = $res->fetch_assoc()) {
     $hasImage = !empty($r['image']) ? 1 : 0;
-    $thumb = $hasImage ? '<img src="includes/item_image_get.php?id='.$r['id'].'" style="max-height:40px;cursor:pointer" class="item-thumb" data-id="'.$r['id'].'" data-hasimage="1">'
-        : '<div class="bg-secondary text-white text-center" style="width:40px;height:40px;line-height:40px;">-</div>';
-    $html.='<tr data-id="'.$r['id'].'">
+
+    // thumbnail: винаги .item-thumb, data-id винаги, data-hasimage=1 или 0
+    if ($hasImage) {
+        $thumb = '<img src="includes/item_image_get.php?id='.$r['id'].'" 
+                        style="max-height:40px;cursor:pointer" 
+                        class="item-thumb" 
+                        data-id="'.$r['id'].'" 
+                        data-hasimage="1">';
+    } else {
+        $thumb = '<div class="item-thumb bg-secondary text-white text-center" 
+                        style="width:40px;height:40px;line-height:40px;cursor:pointer" 
+                        data-id="'.$r['id'].'" 
+                        data-hasimage="0">-</div>';
+    }
+
+    $html .= '<tr data-id="'.$r['id'].'">
         <td>'.$r['nom_code'].'</td>
         <td>'.$r['name'].'</td>
         <td>'.$r['is_calc'].'</td>

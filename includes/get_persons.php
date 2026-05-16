07af5f8cda2ad3632e9778116b512c7d9e756970
cheffoliny <cheffoliny@email>
2026-05-16 21:31:46 +0300

    USERS/ ROLES AND ETC...

--- includes/get_persons.php
+++ includes/get_persons.php
@@ -11,26 +11,26 @@ $db = db_connect('personnel');
 $db_sod = db_connect('sod');
 
 $sql = "
-SELECT
-    p.id,
-    p.fname,
-    p.lname,
-    p.status,
-    aa.id AS account_id,
-    aa.username,
-    aa.id_profile,
-    GROUP_CONCAT(DISTINCT aof.id_office) AS offices_ids,
-    COALESCE(GROUP_CONCAT(DISTINCT offs.name SEPARATOR ', '),'—') AS office_name
-FROM personnel p
-LEFT JOIN ".DB_NAMES['system'].".access_account aa
-    ON p.id = aa.id_person
-LEFT JOIN ".DB_NAMES['system'].".account_office aof
-    ON aa.id = aof.id_account
-LEFT JOIN ".DB_NAMES['sod'].".offices offs
-    ON aof.id_office = offs.id
-GROUP BY p.id
-ORDER BY p.fname, p.lname ASC
-";
+        SELECT
+            p.id,
+            p.fname,
+            p.lname,
+            p.status,
+            aa.id AS account_id,
+            aa.username,
+            aa.id_profile,
+            GROUP_CONCAT(DISTINCT aof.id_office) AS offices_ids,
+            COALESCE(GROUP_CONCAT(DISTINCT offs.name SEPARATOR ', '),'—') AS office_name
+        FROM personnel p
+        LEFT JOIN ".DB_NAMES['system'].".access_account aa
+            ON p.id = aa.id_person
+        LEFT JOIN ".DB_NAMES['system'].".account_office aof
+            ON aa.id = aof.id_account
+        LEFT JOIN ".DB_NAMES['sod'].".offices offs
+            ON aof.id_office = offs.id
+        GROUP BY p.id
+        ORDER BY p.fname, p.lname ASC
+        ";
 
 $result = $db->query($sql);
 
@@ -39,6 +39,8 @@ $html = '';
 while($row = $result->fetch_assoc()){
 
     $id = (int)$row['id'];
+    $officesArr = array_filter(explode(',', $row['offices_ids'] ?? ''));
+    $offices = json_encode(array_values($officesArr));
 
     $html .= '
     <div class="card mb-2 shadow-sm border-0 person-item"
@@ -63,13 +65,13 @@ while($row = $result->fetch_assoc()){
                 </div>
             </div>
 
+
             <button class="btn btn-sm btn-outline-primary openUserModal"
                     data-person-id="'.$id.'"
                     data-account-id="'.(int)$row['account_id'].'"
                     data-username="'.htmlspecialchars($row['username'] ?? '').'"
                     data-profile="'.(int)$row['id_profile'].'"
-                    data-offices=\'["'.($row['offices_ids'] ?? '').'"]\'>
-                    data-offices='<?= json_encode(explode(",", $row['offices_ids'] ?? "")) ?>'>
+                    data-offices=\''.$offices.'\'>
                 <i class="fa fa-key"></i>
             </button>
 

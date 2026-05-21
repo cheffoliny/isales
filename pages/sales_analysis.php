f7a19898be35cc8c55604c7de3e18e56380b40ad
cheffoliny <cheffoliny@email>
2026-05-21 21:48:04 +0300

    UPDATED

--- pages/sales_analysis.php
+++ pages/sales_analysis.php
@@ -135,7 +135,7 @@ if ($selectedOffice > 0) {
 $sql .= "
     WHERE
         pe.to_arc = 0
-        AND p.source_date >= CURDATE() - INTERVAL ? DAY
+        AND DATE(p.source_date) >= DATE_ADD(NOW(), INTERVAL - ? DAY)
 ";
 
 /*
